9b92120d3a3b11b055f229b4b32f526205a3e81c
vldslvD <dobritsavlad@gmail.com>
2023-08-08 23:51:57 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,56 +5,7 @@ import { Notification } from 'components/Notification/Notification';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Section } from 'components/Section/Section';
 import { Container } from './App.styled';
-// class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
 
-//   handleClick = e => {
-//     let temp = e.currentTarget.name;
-//     this.setState(prevState => ({
-//       [temp]: prevState[temp] + 1,
-//     }));
-//   };
-//   countTotalFeedback = () => {
-//     return this.state.good + this.state.neutral + this.state.bad;
-//   };
-//   countPositiveFeedbackPercentage = () => {
-//     return this.countTotalFeedback() === 0
-//       ? 0
-//       : (this.state.good * 100) / this.countTotalFeedback();
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     return (
-//       <Container>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={Object.keys(this.state)}
-//             onLeaveFeedback={this.handleClick}
-//           ></FeedbackOptions>
-//         </Section>
-//         {this.countTotalFeedback() === 0 ? (
-//           <Notification message="There is no feedback"></Notification>
-//         ) : (
-//           <Section title="Statistics">
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={this.countTotalFeedback()}
-//               positivePercentage={this.countPositiveFeedbackPercentage()}
-//             />
-//           </Section>
-//         )}
-//       </Container>
-//     );
-//   }
-// }
-// export default App;
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
